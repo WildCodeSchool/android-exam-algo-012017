0e8b19c8828ea967a0fd474f4aabfff7663d03e9
helenavan <tedone.bd@gmail.com>
2017-01-23 12:35:42 +0100

    bof

--- pates.java
+++ pates.java
@@ -1,5 +1,7 @@
 class Foo {
-	public static String[] potion(String formule){
+	public static String[] poeme(String lettres){
+		lettres = "reigstrrne";
+		String[] tab = lettres.split(" ");
 		
 	}
 }
